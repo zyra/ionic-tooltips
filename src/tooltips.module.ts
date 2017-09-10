e270911320337043ef1c32129ed264cc9321eea9
Jermaine McFarlane <j.mcfarlane.mbbs@gmail.com>
2017-09-10 13:19:34 -0400

    feat: export child module from separate file

--- src/tooltips.module.ts
+++ src/tooltips.module.ts
@@ -1,35 +1,9 @@
+import { TooltipsChildModule, childArgs } from './tooltips.child.module';
 import { NgModule, ModuleWithProviders } from '@angular/core';
-import { IonicModule } from 'ionic-angular';
-import { Tooltip } from './tooltip.directive';
-import { TooltipBox } from './tooltip-box.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-const childArgs: NgModule = {
-  entryComponents: [
-    TooltipBox
-  ],
-    declarations: [
-        Tooltip,
-        TooltipBox,
-    ],
-    imports: [
-      IonicModule
-    ],
-    exports: [
-      Tooltip
-    ]
-};
 const rootArgs: NgModule = Object.assign({}, childArgs);
 rootArgs.imports.push(BrowserAnimationsModule);
 
-@NgModule(childArgs)
-export class TooltipsChildModule {}
-
 @NgModule(rootArgs)
-export class TooltipsModule {
-  static forChild(): ModuleWithProviders {
-    return {
-      ngModule: TooltipsChildModule
-    }
-  }
-}
+export class TooltipsModule {}
