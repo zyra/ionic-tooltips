51e5a5e279dad7ffe7c8eaee18bb87aa18a93fc1
Jermaine McFarlane <j.mcfarlane.mbbs@gmail.com>
2017-09-10 12:45:56 -0400

    feat: allow lazy loading with previous animation import

--- src/tooltips.module.ts
+++ src/tooltips.module.ts
@@ -1,10 +1,10 @@
-import { NgModule } from '@angular/core';
+import { NgModule, ModuleWithProviders } from '@angular/core';
 import { IonicModule } from 'ionic-angular';
 import { Tooltip } from './tooltip.directive';
 import { TooltipBox } from './tooltip-box.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
-@NgModule({
+const childArgs: NgModule = {
   entryComponents: [
     TooltipBox
   ],
@@ -13,11 +13,23 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
         TooltipBox,
     ],
     imports: [
-      IonicModule,
-      BrowserAnimationsModule
+      IonicModule
     ],
     exports: [
       Tooltip
     ]
-})
-export class TooltipsModule {}
+};
+const rootArgs: NgModule = Object.assign({}, childArgs);
+rootArgs.imports.push(BrowserAnimationsModule);
+
+@NgModule(childArgs)
+export class TooltipsChildModule {}
+
+@NgModule(rootArgs)
+export class TooltipsModule {
+  static forChild(): ModuleWithProviders {
+    return {
+      ngModule: TooltipsChildModule
+    }
+  }
+}
